80a25c28d6a0d0d4edcf48eb809164126b3557a6
hadrien <hhoutmann@gmail.com>
2020-09-03 22:44:31 +0200

    add vuejs front

---
+++ apps-js/vuetest/src/store/mutations-types.js
@@ -0,0 +1,11 @@
+import _ from 'lodash'
+
+const createAsyncMutation = (type) => ({
+    SUCCESS: `${type}_SUCCESS`,
+    FAILURE: `${type}_FAILURE`,
+    PENDING: `${type}_PENDING`,
+    loadingKey: _.camelCase(`${type}_PENDING`),
+    stateKey: _.camelCase(`${type}_DATA`)
+})
+
+export const GET_USER_INFO_ASYNC = createAsyncMutation('GET_USER_INFO')
