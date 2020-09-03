ffc695a15f514d4d430ff377eefac76ef55d8d08
hadrien <hhoutmann@gmail.com>
2020-09-03 22:44:39 +0200

    add vuejs front

---
+++ apps-js/vuetest/src/modules/core/Spinner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <md-progress-spinner class="md-accent" :md-diameter="30" md-mode="indeterminate"></md-progress-spinner>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Spinner'
+    }
+</script>
+
+<style lang="scss">
+    .md-progress-spinner {
+        margin: 24px;
+    }
+</style>
