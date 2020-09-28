b718ea908395653ff9ad2f3b49f03036568e65f1
Had <hhoutmann@gmail.com>
2020-09-28 08:13:17 +0200

    Update settings.py

--- kineproject/settings.py
+++ kineproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(@s#i-t6%rnh87y7a#7br@*qh3eg^$va5=kabza^kl$jrb-n)&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
